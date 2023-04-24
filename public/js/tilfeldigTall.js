7721b747d4b4c5cba7eddbada5c39992dd6bd27a
sonadro <sondre.rooth@gmail.com>
2023-04-24 11:51:23 +0200

    Bruker kan gjette på lykketall
    Hvis de gjetter riktig bytter bakgrunnen farge

--- public/js/tilfeldigTall.js
+++ public/js/tilfeldigTall.js
@@ -1,14 +1,55 @@
 // DOM
 const tallElement = document.querySelector('.tallElement');
+const gjettTallForm = document.querySelector('.gjettTallForm');
+const gjettTallTekst = gjettTallForm.querySelector('.gjettTallTekst');
+const dittTall = gjettTallForm.querySelector('.dittTall');
+const body = document.querySelector('body');
+const lykkeFarge = document.querySelector('.lykkeFarge');
+
+// variabler
+let brukerensTall = -1;
+let tilfeldigTall = Math.floor(Math.random() * 10) + 1;
+let tilfeldigFarge;
+const tilfeldigeFarger = ['green', 'red', 'pink', 'teal', 'yellow'];
+
+// brukeren kan gjette det neste tallet
+gjettTallForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    brukerensTall = Number(gjettTallForm.tallInput.value);
+    dittTall.innerText = brukerensTall;
+    gjettTallTekst.classList.remove('hidden');
+});
 
 // når siden lastes, generer tilfeldig tall
-tallElement.innerText = Math.floor(Math.random() * 99) + 1;
+tallElement.innerText = tilfeldigTall
 
 // generer nytt tilfeldig tall hvert minutt
 setInterval(() => {
     let sekunder = new Date().getSeconds();
 
     if (sekunder === 0) {
-        tallElement.innerText = Math.floor(Math.random() * 99) + 1;
-    }
+        let forrigeTall = tilfeldigTall;
+
+        // pass på at det aldri blir samme tall 2 ganger på rad
+        while (forrigeTall === tilfeldigTall) {
+            tilfeldigTall = Math.floor(Math.random() * 10) + 1;
+        };
+
+        tallElement.innerText = tilfeldigTall;
+
+        // hvis bruker gjetter riktig tall
+        if (brukerensTall === tilfeldigTall) {
+            const forrigeFarge = tilfeldigFarge;
+
+            // samme farge kommer ikke 2 ganger på rad
+            while(tilfeldigFarge === forrigeFarge) {
+                tilfeldigFarge = tilfeldigeFarger[Math.floor(Math.random() * tilfeldigeFarger.length) + 1];
+            };
+
+            // bytt bakgrunnsfarge og vis lykkeFarge-teksten
+            body.style.backgroundColor = tilfeldigFarge;
+            lykkeFarge.classList.remove('hidden');
+        };
+    };
 }, 1000);
